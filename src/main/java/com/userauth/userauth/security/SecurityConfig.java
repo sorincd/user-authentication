705382f93653b73cf9d8b0808eab084a82c50f02
sorind <constantinsorin.damian@vdab.be>
2021-01-17 20:47:00 +0200

    [Sorin] - cleanup code

--- src/main/java/com/userauth/userauth/security/SecurityConfig.java
+++ src/main/java/com/userauth/userauth/security/SecurityConfig.java
@@ -4,7 +4,6 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -18,19 +17,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-      /*  http
-                .csrf().disable()
-                .authorizeRequests()
-                .antMatchers("/", "/login*").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .formLogin()
-                .loginPage("/login.html")
-                .permitAll()
-                .and()
-                .logout()
-                .permitAll();*/
-
         http
                 .csrf().disable()
                 .authorizeRequests()
